7fa0a1f239da657e9c5a035851448e89e0413428
Ross Fletcher and Sam Gespass <ross@>
2017-12-06 11:32:09 -0800

    recognize qu as a consonant

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,21 @@ function pigLatin(input) {
   console.log(array);
   var re = /\b[A-Z]/gi;
   var reVowel = /\b[aeiou]/gi;
-  var reFirstLetter
+  var reQu = /\b(qu)/gi;
   var newArray = [];
   array.forEach(function(value){
     if (value.search(re) !== -1 && value.search(reVowel) !== -1) {
       value = value + "way";
       newArray.push(value);
     } else if (value.search(re) !== -1 && value.search(reVowel) === -1) {
-      var first = value[0];
-      value = value.substr(1) + first + "ay";
-      newArray.push(value);
+      if (value.search(reQu) !== -1) {
+        value = value.substr(2) + "quay";
+        newArray.push(value);
+      } else {
+        var first = value[0];
+        value = value.substr(1) + first + "ay";
+        newArray.push(value);
+      }
     } else {
       newArray.push(value);
     } console.log(value);
